6ca3882fc13f24d05c42adc2d0c7da03f9b6f9d3
KajetanZ <kajetan5e@gmail.com>
2021-04-30 09:20:36 +0200

    add cors to server

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const { port } = require('./config/appConfig');
 const app = express();
 const tasksRouter = require('./routes/api/tasks');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('./db/connection');
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/api/tasks', tasksRouter);
 
 app.listen(port, console.log(`Task Manager HTTP server is running: http://127.0.0.1:${port}`));
