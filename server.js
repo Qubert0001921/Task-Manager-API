ba0ad7316480790d983e83d8373b1dec8947c6a1
KajetanZ <kajetan5e@gmail.com>
2021-05-14 22:40:32 +0200

    add dotenv to server

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 const config = require('./config/appConfig');
 const app = express();
@@ -13,7 +15,9 @@ app.use(cors());
 app.use('/api/tasks', tasksRouter);
 
 app.listen(config.port, () => {
-    console.log("( Task Manager API )")
-    console.log(`Node.js HTTP server is running: http://127.0.0.1:${config.port}`);
+    console.log("\x1b[1m( Task Manager API )\x1b[0m")
+    console.log("\x1b[32mSuccessfully started HTTP server!\x1b[0m\n")
+    console.log(`Node.js HTTP server is running: http://127.0.0.1:${config.port}/api/tasks`);
     console.log(`Database connection: ${config.database}`);
+    console.log("\n\x1b[36mPress CTRL+C or close terminal window to exit process\x1b[0m")
 });
